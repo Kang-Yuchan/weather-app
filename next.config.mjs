aa7c0483b82739694261b6d2980ee9b7d9ca0015
Kang-Yuchan <mikuplayer123@gmail.com>
2024-09-11 04:43:36 +0900

    chore: update next.config for using hosting images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.weatherapi.com',
+        port: '',
+        pathname: '/weather/**',
+      },
+    ],
+  },
   sassOptions: {
     prependData: `@import "@/styles/variables.scss";`,
   },
