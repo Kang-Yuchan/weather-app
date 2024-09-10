9f4987e1af3101c51391f63c4a966b14b5908ae9
Kang-Yuchan <mikuplayer123@gmail.com>
2024-09-10 18:50:30 +0900

    feat: add ui Custom Input component

---
+++ src/components/ui/Input/index.tsx
@@ -0,0 +1,17 @@
+import React, { forwardRef } from 'react';
+import classNames from 'classnames';
+import styles from './index.module.scss';
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
+  return (
+    <div className={classNames(styles.inputWrapper, className)}>
+      <input ref={ref} className={styles.input} {...props} />
+    </div>
+  );
+});
+
+Input.displayName = 'Input';
+
+export default Input;
