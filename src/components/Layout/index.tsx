a05dda04d064a6b3c94b5209971557aadc8e886f
Kang-Yuchan <mikuplayer123@gmail.com>
2024-09-13 03:43:00 +0900

    feat: add Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import styles from './index.module.scss';
+import classNames from 'classnames';
+
+type LayoutProps = {
+  title: string;
+  className?: string;
+  children: ReactNode;
+};
+
+const Layout = ({ title, className, children }: LayoutProps) => {
+  return (
+    <main className={classNames(styles.layout, className)}>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.container}>{children}</div>
+    </main>
+  );
+};
+
+export default Layout;
