db7fefdb8ad9dd2e79b0dd6b8bbf5ebdc9ca4bce
maku <mikuplayer123@gmail.com>
2024-09-13 03:10:16 +0900

    Chore/update weather types (#8)
    * chore: add github actions workflow CI/CD
    
    * fix: github actions workflow name
    
    * chore: update ci yml
    
    * chore: update weather.ts

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -52,15 +52,10 @@ export type ForecastResponse = WeatherResponse & {
 
 export type ForecastDay = {
   date: string;
-  day: {
-    maxtemp_c: number;
-    mintemp_c: number;
-    condition: {
-      text: string;
-      icon: string;
-      code: number;
-    };
-  };
+  date_epoch: number;
+  day: DayData;
+  astro: AstroData;
+  hour: HourData[];
 };
 
 export type FutureResponse = Omit<ForecastResponse, 'current'>;
@@ -76,3 +71,80 @@ export type LocationResponse = {
 };
 
 export type WeatherQuery = string | { lat: string; lon: string };
+
+type DayData = {
+  maxtemp_c: number;
+  maxtemp_f: number;
+  mintemp_c: number;
+  mintemp_f: number;
+  avgtemp_c: number;
+  avgtemp_f: number;
+  maxwind_mph: number;
+  maxwind_kph: number;
+  totalprecip_mm: number;
+  totalprecip_in: number;
+  totalsnow_cm: number;
+  avgvis_km: number;
+  avgvis_miles: number;
+  avghumidity: number;
+  daily_will_it_rain: number;
+  daily_chance_of_rain: number;
+  daily_will_it_snow: number;
+  daily_chance_of_snow: number;
+  condition: ConditionData;
+  uv: number;
+};
+
+type AstroData = {
+  sunrise: string;
+  sunset: string;
+  moonrise: string;
+  moonset: string;
+  moon_phase: string;
+  moon_illumination: number;
+  is_moon_up: number;
+  is_sun_up: number;
+};
+
+type HourData = {
+  time_epoch: number;
+  time: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: ConditionData;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  snow_cm: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  windchill_c: number;
+  windchill_f: number;
+  heatindex_c: number;
+  heatindex_f: number;
+  dewpoint_c: number;
+  dewpoint_f: number;
+  will_it_rain: number;
+  chance_of_rain: number;
+  will_it_snow: number;
+  chance_of_snow: number;
+  vis_km: number;
+  vis_miles: number;
+  gust_mph: number;
+  gust_kph: number;
+  uv: number;
+};
+
+type ConditionData = {
+  text: string;
+  icon: string;
+  code: number;
+};
